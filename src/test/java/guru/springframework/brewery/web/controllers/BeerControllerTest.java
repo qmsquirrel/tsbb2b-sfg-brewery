2749612ac56eca0bdfc567766bcbe0ade53fcd04
Ralph L <lettau.ralph@gmail.com>
2019-06-20 11:45:19 +0200

    Added Beercontroller with setup

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -0,0 +1,53 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.services.BeerService;
+import guru.springframework.brewery.web.model.BeerDto;
+import guru.springframework.brewery.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BeerControllerTest {
+
+    @Mock
+    BeerService beerService;
+
+    @InjectMocks
+    BeerController beerController;
+
+    MockMvc mockMvc;
+
+    BeerDto validBeer;
+
+    @BeforeEach
+    void setUp() {
+        validBeer = BeerDto.builder().id(UUID.randomUUID())
+                .version(1)
+                .beerName("Beer1")
+                .beerSytle(BeerStyleEnum.PALE_ALE)
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(4)
+                .upc(123456789012L)
+                .createdDate(OffsetDateTime.now())
+                .lastModifiedDate(OffsetDateTime.now())
+                .build();
+
+        mockMvc = MockMvcBuilders.standaloneSetup(beerController).build();
+    }
+
+    @Test
+    void getBeerById() {
+    }
+}
